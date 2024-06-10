f2ff7c301f02368e28cc3be50a1eff1b36dc20cd
ademeyer <adebayotimileyin@gmail.com>
2024-06-10 16:26:21 +0100

    Create Bit Manipulation sln folder

---
+++ NumberOfOneBits/NumberOfOneBits.cpp
@@ -0,0 +1,18 @@
+// NumberOfOneBits.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class Solution
+{
+public:
+    int hammingWeight(int n)
+    {
+
+    }
+};
+
+int main()
+{
+}
